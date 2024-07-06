1adb247900aaab96c59d53196cd134ceac24e1e2
rahat9016 <minhajurrohoman9016@gmail.com>
2024-07-06 21:54:50 +0600

    init boilderplate

---
+++ src/redux/api/apiSlice.ts
@@ -0,0 +1,47 @@
+import {
+  BaseQueryApi,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react'
+import { getUserData } from '../../utility/getUser'
+import { logout, setToken, setUserInfo } from '../feature/auth/authAction'
+import { refreshAuthToken } from '../feature/auth/authSlice'
+
+
+const baseQuery = async (
+  args: string | FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: object,
+) => {
+  const result = await fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL,
+  })(args, api, extraOptions)
+  if (result?.error?.status === 401) {
+    const refreshResult = await api.dispatch(refreshAuthToken()).unwrap()
+    if (refreshResult) {
+      // token set in setToken
+      api.dispatch(setToken(refreshResult.access))
+      // decode user and data store in setUserInfo
+      api.dispatch(setUserInfo(getUserData(refreshResult.access)))
+
+      return await fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL })(
+        args,
+        api,
+        extraOptions,
+      )
+    } else {
+      api.dispatch(logout())
+    }
+  } else if (result?.error?.status === 'FETCH_ERROR') {
+    api.dispatch(logout())
+  }
+  return result
+}
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: baseQuery as any,
+  tagTypes: [],
+  endpoints: () => ({}),
+})
---
+++ src/redux/store.ts
@@ -0,0 +1,54 @@
+import { EnhancedStore, StoreEnhancer, ThunkDispatch, Tuple, UnknownAction, configureStore } from "@reduxjs/toolkit"
+import { getUserData } from "../utility/getUser"
+import { IInitialState } from "./feature/auth/authAction"
+import { CombinedState } from "@reduxjs/toolkit/query"
+import { api } from "./api/apiSlice"
+import authReducer from "../redux/feature/auth/authAction"
+
+const token = localStorage.getItem('token')
+
+const initialState: IInitialState = {
+  loading: false,
+  userInfo: getUserData(token),
+  userToken: token,
+  error: null,
+  success: false,
+}
+
+const store: EnhancedStore<
+  {
+    api: CombinedState<{}, never, 'api'>
+    auth: IInitialState
+  },
+  UnknownAction,
+  Tuple<
+    [
+      StoreEnhancer<{
+        dispatch: ThunkDispatch<
+          {
+            api: CombinedState<{}, never, 'api'>
+            auth: IInitialState
+          },
+          undefined,
+          UnknownAction
+        >
+      }>,
+      StoreEnhancer,
+    ]
+  >
+> = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    auth: authReducer,
+  },
+  preloadedState: {
+    auth: initialState,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(api.middleware),
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
