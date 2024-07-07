32a1634369f8fd97608b05db121c72ad07b5f733
I561603 <mahiuddin.al.kamal@sap.com>
2024-07-07 10:35:10 +0200

    fixed issues

--- src/routes/Protected.tsx
+++ src/routes/Protected.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Navigate, useLocation } from "react-router-dom";
+// import { Navigate, useLocation } from "react-router-dom";
 // import { useAppSelector } from "../redux/hooks";
 
 const Protected = ({ children }: { children: any }) => {
