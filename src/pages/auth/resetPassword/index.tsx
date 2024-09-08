3eae2c3a59488d5f61e1fc872d8df93ae92e8e56
rahat9016 <minhajurrohoman9016@gmail.com>
2024-09-08 19:49:38 +0600

    reset password logic working

--- src/pages/auth/resetPassword/index.tsx
+++ src/pages/auth/resetPassword/index.tsx
@@ -1,10 +1,88 @@
+import { useState } from "react";
+import authVector from "../../../assets/vectorAuth.png";
 import Image from "../../../components/shared/Image";
 import Input from "../../../components/shared/Input";
-import authVector from "../../../assets/vectorAuth.png"
 
+
+interface ILoginForm {
+    email: string;
+    oldPassword: string;
+    newPassword: string;
+    rePassword: string;
+}
 export default function ResetPassword() {
-  return (
-    <div>
+    const [errors, setErrors] = useState<{
+        [key: string]: string | null | undefined;
+    }>({});
+
+    const [resetInfo, setResetInfo] = useState<ILoginForm>({
+        email: "",
+        oldPassword: "",
+        newPassword: "",
+        rePassword: ""
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // login information store in state
+        setResetInfo({ ...resetInfo, [e.target.name]: e.target.value });
+        setErrors({
+            ...errors,
+            [e.target.id]: "",
+        });
+    };
+    const validateForm = () => {
+        let errors: {
+            email: string | null | undefined;
+            oldPassword: string | null | undefined;
+            newPassword: string | null | undefined;
+            rePassword: string | null | undefined;
+        } = {
+            email: undefined,
+            oldPassword: undefined,
+            newPassword: undefined,
+            rePassword: undefined
+        };
+        // Regular expression to validate email format
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!resetInfo.email.trim()) {
+            errors.email = "E-mail is required";
+        } else if (!emailRegex.test(resetInfo.email)) {
+            errors.email = "Please enter a valid email address";
+        }
+        if (!resetInfo.oldPassword) {
+            errors.oldPassword = "Password is required";
+        }
+        if (!resetInfo.newPassword) {
+            errors.newPassword = "New password is required";
+        } else if (resetInfo.oldPassword.length < 0) {
+            errors.newPassword = "New Password must be at least 6 characters long";
+        }
+        if (!resetInfo.rePassword) {
+            errors.rePassword = "Confirm password is required";
+        } else if (resetInfo.oldPassword.length < 0) {
+            errors.rePassword = "Confirm password must be at least 6 characters long";
+        }
+        setErrors(errors);
+
+        const hasErrors = Object.values(errors).some(
+            (error) => error !== undefined
+        );
+        return !hasErrors;
+    };
+
+    console.log(errors)
+    const handleSubmit = async (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+        if (validateForm()) {
+            try {
+
+            } catch (error) {
+                console.log("login error", error);
+            }
+        }
+    };
+    return (
+        <div>
             <div className="container h-[777px] bg-white shadow-custom-box-shadow flex items-center mb-10">
                 <div className="w-1/2 flex items-center justify-center">
                     <Image src={authVector} alt="Book Vector" />
@@ -15,57 +93,58 @@ export default function ResetPassword() {
                             Reset Password
                         </h2>
                         <form
-                            // onSubmit={handleSubmit}
-                            className="flex flex-col gap-4"
+                            onSubmit={handleSubmit}
+                            className="flex flex-col gap-1"
                         >
-                            <div className="relative">
-                                <Input
-                                type="password"
-                                    // type={showPassword ? "text" : "password"}
-                                    // id="password"
-                                    // value={userInfo.password}
-                                    // name="password"
-                                    // onChange={handleChange}
-                                    // placeholder="Password"
-                                    // error={errors.password}
-                                />
-                                <div
-                                    className="absolute right-4 top-[18px] flex items-center cursor-pointer"
-                                    // onClick={() =>
-                                    //     setShowPassword(!showPassword)
-                                    // }
-                                >
-                                    {/* {showPassword ? (
-                                        <PiEyeClosed className=" text-grayDark" />
-                                    ) : (
-                                        <AiOutlineEye className=" text-grayDark" />
-                                    )} */}
-                                </div>
-                            </div>
-                           
+                            <Input
+                                type='text'
+                                id="email"
+                                value={resetInfo.email}
+                                name="email"
+                                onChange={handleChange}
+                                placeholder="E-mail"
+                                error={errors.email}
+                            />
+                            <Input
+                                type='text'
+                                id="oldPassword"
+                                value={resetInfo.oldPassword}
+                                name="oldPassword"
+                                onChange={handleChange}
+                                placeholder="Old Password"
+                                error={errors.oldPassword}
+                            />
+                            <Input
+                                type='text'
+                                id=""
+                                value={resetInfo.newPassword}
+                                name="newPassword"
+                                onChange={handleChange}
+                                placeholder="New Password"
+                                error={errors.newPassword}
+                            />
+                            <Input
+                                type='text'
+                                id=""
+                                value={resetInfo.rePassword}
+                                name="rePassword"
+                                onChange={handleChange}
+                                placeholder="Confirm Password"
+                                error={errors.rePassword}
+                            />
+
+
                             <button
                                 type="submit"
-                                className="w-full px-4 py-2 font-bold text-white bg-primary rounded-md "
+                                className="w-full px-4 py-2 font-bold text-white bg-primary rounded-md mt-3"
                             >
-                                Sign In
+                                Reset Password
                             </button>
-                            <div className=" flex items-center gap-3 mt-6">
-                                <p className="text-grayDark text-sm font-normal">
-                                    Don’t have an account yet ?
-                                </p>
-                                <button
-                                    className="text-primary font-medium text-sm"
-                                    onClick={(e) => {
-                                        e.preventDefault();
-                                    }}
-                                >
-                                    Sign Up
-                                </button>
-                            </div>
+                            
                         </form>
                     </div>
                 </div>
             </div>
         </div>
-  )
+    )
 }
