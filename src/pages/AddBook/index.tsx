f9d1a554bb38e202043ff75dddd3ff08ef0209a6
rahat9016 <minhajurrohoman9016@gmail.com>
2024-12-11 20:15:08 +0600

    add: implemented UI of my library

--- src/pages/AddBook/index.tsx
+++ src/pages/AddBook/index.tsx
@@ -1,6 +1,30 @@
-
+import { useNavigate } from "react-router-dom"
+import Image from "../../components/shared/Image"
+import Button from "../../components/shared/Button"
+import leftArrowIcon from "../../assets/leftArrow.png"
 export default function AddBook() {
+  const navigate = useNavigate()
   return (
-    <div>AddBook</div>
+    <div>
+      <div className="fixed left-0 top-0 w-full h-[48px] flex items-center justify-between px-4 border-b border-[#E4E4E4] bg-light z-30 ">
+                <div className="flex items-center gap-2">
+                    <div
+                        className="cursor-pointer w-5"
+                        onClick={() => navigate("/profile/user-profile")}
+                    >
+                        <Image src={leftArrowIcon} alt="left" />
+                    </div>
+                    <h3 className="font-sofia text-base font-medium ">
+                        Edit Profile
+                    </h3>
+                </div>
+                <div>
+                    <Button
+                    >
+                        Save
+                    </Button>
+                </div>
+            </div>
+    </div>
   )
 }
--- src/pages/profile/components/AddBookComponent.tsx
+++ src/pages/profile/components/AddBookComponent.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import plus from "../../../assets/plus.png";
 import Image from "../../../components/shared/Image";
 export default function AddBookComponent() {
+    const navigate = useNavigate();
     return (
-        <div className="max-w-[168px] h-[264px] flex flex-col items-center justify-center gap-2 border border-grayDark border-dashed">
+        <div
+            onClick={() => navigate("/profile/add-book")}
+            className="max-w-[168px] h-[264px] flex flex-col items-center justify-center gap-2 border border-grayDark border-dashed"
+        >
             <Image src={plus} alt="Plus" />
-            <p className="font-sofia text-sm font-medium text-grayDark">Add a book</p>
+            <p className="font-sofia text-sm font-medium text-grayDark">
+                Add a book
+            </p>
         </div>
     );
 }
