97acf707fa2d16b66be0213186ce1a333c0818fa
rahat9016 <minhajurrohoman9016@gmail.com>
2025-01-06 21:29:28 +0600

    fix: books api logic

--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -1,23 +1,26 @@
+import BookCard from "../../components/shared/BookCard";
+import BookSkeleton from "../../components/shared/skeleton/BookSkeleton";
 import { useGetAllBooksQuery } from "../../redux/feature/book/bookApi";
 import { goToTop } from "../../utility/helper";
+import { IBook } from "./interface";
 
 export default function Books() {
-    const { data, isError } = useGetAllBooksQuery(undefined);
+    const { data, isError, isLoading } = useGetAllBooksQuery(undefined);
     goToTop();
-    if(isError) return 
     console.log(data)
+    if(isError) return <p>Something went wrong</p>
     return (
         <section>
             <div className="container min-h-[80vh]">
                 <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-6 gap-2">
-                    {/* {isLoading
+                    {isLoading
                         ? Array.from({ length: 10 }, (_, index) => (
                               <BookSkeleton key={index} />
                           ))
-                        : booksData &&
-                          booksData?.map((book: IBook, index: number) => (
+                        : data &&
+                        data?._embedded?.books?.map((book: IBook, index: number) => (
                               <BookCard key={index} book={book} />
-                          ))} */}
+                          ))}
                 </div>
             </div>
         </section>
