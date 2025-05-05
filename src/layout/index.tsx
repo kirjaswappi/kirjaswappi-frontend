0537a90cca9fae852c510cb9935a1ff4e53a95cc
Minhajur Rohoman <minhajurrohoman9016@gmail.com>
2025-05-05 20:00:30 +0600

    Style: Swap Book carousels

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import SwapModal from "../pages/bookDetails/components/SwapModal";
-
 export default function Layout() {
     return (
         <div className="bg-light ">
             <Header />
             <main>
-                <SwapModal/>
                 <Outlet />
             </main>
             <Footer />
